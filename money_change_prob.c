89d679eaf674ec92054c647861957c6f4944e7a7
Purushottam <kpk62045@gmail.com>
2026-01-22 20:04:17 +0530

    Money change problem

---
+++ money_change_prob.c
@@ -0,0 +1,48 @@
+/* Compute the minimum number of coins required
+   to change the given value using coins of 5, 2, and 1 */
+
+#include <stdio.h>
+
+int main()
+{
+    // assigning Variable
+
+    int amount;
+    int coins5, coins2, coins1;
+    int remaining;
+
+    printf("Enter amount to be changed: ");
+    scanf("%d", &amount);
+
+     if (amount<=0)
+    {
+        printf("Invalid amount");
+        return 0;
+    }
+    
+    else if (scanf("%d", &amount) != 1)
+    {
+        printf("Invalid input! Please enter an integer value.\n");
+        return 0;
+    }
+    else{
+    // Main logic
+
+    coins5 = amount / 5;
+    remaining = amount % 5;
+
+    coins2 = remaining / 2;
+    remaining = remaining % 2;
+
+    coins1 = remaining;
+
+    printf("Rs 5 coins : %d\n", coins5);
+    printf("Rs 2 coins : %d\n", coins2);
+    printf("Rs 1 coins : %d\n", coins1);
+
+    printf("Minimum number of coins = %d\n",
+           coins5 + coins2 + coins1);
+
+    return 0;
+    }
+}
